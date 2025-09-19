2e9cbcaff22362e1a39d99c9c463fadd642e49ab
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-09-19 12:53:12 -0300

    feat: estilizacao da secao dos botoes de login

--- src/components/SecaoBotoesLogin/SecaoBotoesLogin.jsx
+++ src/components/SecaoBotoesLogin/SecaoBotoesLogin.jsx
@@ -1,8 +1,14 @@
+import "./SecaoBotoesLogin.scss"
+import apple_logo from "../../assets/images/apple_logo.png"
+import google_logo from "../../assets/images/google_logo.png"
+
+
+
 function SecaoBotoesLogin() {
   return (
-    <section>
-      <button>Login com Apple</button>
-      <button>Login com Google</button>
+    <section className="login_card__secao_botoes">
+      <button><img src={apple_logo} alt="Ícone" /><p>Login com Apple</p></button>
+      <button><img src={google_logo} alt="Ícone" /><p>Login com Google</p></button>
     </section>
   );
 }
