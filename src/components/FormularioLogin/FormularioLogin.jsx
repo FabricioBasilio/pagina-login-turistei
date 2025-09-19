142abc85e854ba7525a167f0960424965aa8de12
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-09-19 13:32:13 -0300

    feat: ajustes de cores e dinamismo nos botoes de opcoes de entrar

--- src/components/FormularioLogin/FormularioLogin.jsx
+++ src/components/FormularioLogin/FormularioLogin.jsx
@@ -1,9 +1,17 @@
 import "./FormularioLogin.scss";
-import mail from "../../assets/images/mail.png"
-import visibility from "../../assets/images/visibility.png"
-
+import mail from "../../assets/images/mail.png";
+import visibility from "../../assets/images/visibility.png";
+import { useState } from "react";
 
 function FormularioLogin() {
+  const [loginMethod, setLoginMethod] = useState("sign_in");
+
+  function changeMethod(e) {
+    e.target.id === "botao_sign_in"
+      ? setLoginMethod("sign_in")
+      : setLoginMethod("sign_up");
+  }
+
   return (
     <section className="login_card__conteudo">
       <section className="conteudo__titulos">
@@ -12,16 +20,34 @@ function FormularioLogin() {
       </section>
       {/* futuro componente */}
       <div className="conteudo__opcoes_entrar">
-        <button>Login</button>
-        <button>Criar conta</button>
+        <button
+          id="botao_sign_in"
+          className={loginMethod === "sign_in" ? "--active" : ""}
+          onClick={changeMethod}
+        >
+          Login
+        </button>
+        <button
+          id="botao_sign_up"
+          className={loginMethod === "sign_up" ? "--active" : ""}
+          onClick={changeMethod}
+        >
+          Criar conta
+        </button>
       </div>
       <form className="conteudo__formulario">
-        <div className="entrada"><input placeholder="E-mail" type="email" maxLength={30} required /> <img src={mail} alt="Ícone de e-mail"/></div>
-        <div className="entrada"><input placeholder="Senha" type="password" maxLength={10} required /> <img src={visibility} alt="Ícone de visualização"/></div>
+        <div className="entrada">
+          <input placeholder="E-mail" type="email" maxLength={30} required />{" "}
+          <img src={mail} alt="Ícone de e-mail" />
+        </div>
+        <div className="entrada">
+          <input placeholder="Senha" type="password" maxLength={10} required />{" "}
+          <img src={visibility} alt="Ícone de visualização" />
+        </div>
         <section className="formulario__opcoes">
           <div>
-              <input type="radio" id="lembrar_senha" />{" "}
-              <label htmlFor="lembrar_senha">Lembrar minha senha</label>
+            <input type="radio" id="lembrar_senha" />{" "}
+            <label htmlFor="lembrar_senha">Lembrar minha senha</label>
           </div>
           <a href="#">Esqueceu a senha?</a>
         </section>
